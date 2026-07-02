5e81ba168e69ea4328651fc14b679c1835164e16
MooPlugins <manoj@bitkea.com>
2026-07-02 16:49:55 +0530

    Update version.php

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ $plugin->component = 'local_smartsearch';
 $plugin->version = 2026062410;
 $plugin->requires = 2024100700; // Moodle 4.5 or later.
 $plugin->supported = [405, 502]; // Moodle 4.5 through 5.2.
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.9';
